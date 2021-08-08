7294981f4c25aa247e555bc96116edc64abad900
PanOkurka <67430231+PanOkurka@users.noreply.github.com>
2021-08-08 22:04:10 +0200

    Edited some style

--- src/components/homepage/home.js
+++ src/components/homepage/home.js
@@ -11,25 +11,30 @@ function Home() {
         <>
             <div className='wraper'>
             <Particles className='particlesjs' />
-                <div className='centerMain'>
-                    <img className='Avatar' src="OkurkaIcon.jpg" alt="Okurka Icon" />
-                    <div id='Center'>
-                        <h1 id='Name'>Pan_Okurka</h1>
-                        <h2 id='text'>I code in <Typing /></h2>
-                    </div>
-                    <div className='Links'>
-                        <a href='https://github.com/PanOkurka' className='social-icon-link'>
-                            <FaGithub className='github-icon' />
-                        </a>
-                        <a href='https://twitter.com/Pan_Okurka' className='social-icon-link'>
-                            <FaTwitter className='twitter-icon'/>
-                        </a>
-                        <a href='https://www.instagram.com/panokurka_official/' className='social-icon-link'>
-                            <FaInstagram className='instagram-icon' />
-                        </a>
-                        <a href='https://discord.gg/wn5aF2w' className='social-icon-link'>
-                            <FaDiscord className='discord-icon' />
-                        </a>
+                <div className='centerBox'> 
+                    <div className='centerMain'>
+                        <img className='Banner' src='Funny.gif' alt='Funny' />
+                        <div className='ContentMove'>
+                            <img className='Avatar' src="OkurkaIcon.jpg" alt="Okurka Icon" />
+                            <div id='Center'>
+                                <h1 id='Name'>Pan_Okurka</h1>
+                                <h2 id='text'>I code in <Typing /></h2>
+                            </div>
+                            <div className='Links'>
+                                <a href='https://github.com/PanOkurka' className='social-icon-link'>
+                                    <FaGithub className='github-icon' />
+                                </a>
+                                <a href='https://twitter.com/Pan_Okurka' className='social-icon-link'>
+                                    <FaTwitter className='twitter-icon'/>
+                                </a>
+                                <a href='https://www.instagram.com/panokurka_official/' className='social-icon-link'>
+                                    <FaInstagram className='instagram-icon' />
+                                </a>
+                                <a href='https://discord.gg/wn5aF2w' className='social-icon-link'>
+                                    <FaDiscord className='discord-icon' />
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
