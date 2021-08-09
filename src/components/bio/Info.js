a48374ca40c17005a49c2f51ae6bff4cad70dbb3
PanOkurka <67430231+PanOkurka@users.noreply.github.com>
2021-08-09 14:24:34 +0200

    Updated some info about me

--- src/components/bio/Info.js
+++ src/components/bio/Info.js
@@ -10,7 +10,7 @@ function Info() {
             <div class="bio-container">
                     <h1><strong>My Bio:</strong></h1>
                     <div class="bio-container-2">
-                        <p className='p'>Hi! My nickname is <span className='blurpur'>Pan_Okurka</span>. I live in Czech Republic. I make Discord bots, websites and right now working on Atorpol Project's. <br /> <br /> I've been coding around <span className='blurpur'>2 or 3 years</span>. <br /> <br /> If u wanna know new's about my discord bot, u can join my discord server called <span className='blurpur'>"Cucumber Development"</span>. My Discord Tag is <span className='blurpur'>Pan_Okurka#8080</span></p>
+                        <p className='p'>Hi! My nickname is <span className='blurpur'>Pan_Okurka</span>. I live in the Czech Republic. I'm coding Discord bots, websites and right now working on Atorpol Projects. <br /> <br /> I've been coding for almost <span className='blurpur'> 4 years</span>. <br /> <br /> U can check out my discord server called <span className='blurpur'>"Cucumber Development"</span>, where I post news about my discord bot. My Discord Tag is <span className='blurpur'>Pan_Okurka#8080</span></p>
                     </div>
             </div>
         </div>
