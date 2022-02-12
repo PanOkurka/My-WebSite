24a8104fdf09ca344f933cbf5e5e966fff56feee
Pan_Okurka <67430231+PanOkurka@users.noreply.github.com>
2022-02-12 09:03:48 +0100

    some corection

---
+++ pages/projects.js
@@ -0,0 +1,84 @@
+import Layout from '../components/layout'
+import ParticlesComponent from '../components/paticlecomponent'
+import CardDeck from 'react-bootstrap/CardGroup'
+import Card from 'react-bootstrap/Card'
+
+export default function Projects() {
+  return (
+    <Layout>
+       <>
+              <ParticlesComponent />
+              <div className='center2'>
+                <div className='project-container'>
+                    <div className='space-line'>
+                    <CardDeck>
+                    <Card>
+                        <Card.Img variant="top" src="/images/Dragonex.png" />
+                        <Card.Body>
+                        <Card.Title>Dragonex</Card.Title>
+                        <Card.Text>
+                            A server rendering web for CZ/SK minecraft server. <br /><br />
+                            <a href='https://dragonex.eu' className='FixedTest' target='_self'>Link Here</a>
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src="/images/OkurkaCraftSite.png" />
+                        <Card.Body>
+                        <Card.Title>OkurkaCraft Site</Card.Title>
+                        <Card.Text>
+                            Just my little website for my Minecraft Server!<br /><br />
+                            <a href='/' className='FixedTest' target='_self'>Link Here (Off)</a>
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src="/images/BabyCucumberSite.png" />
+                        <Card.Body>
+                        <Card.Title>Baby-Cucumber Site</Card.Title>
+                        <Card.Text>
+                            A website for my Discord bot!<br></br> 
+                            If u wanna know more just join my discord!<br /><br />
+                            <a href='https://baby-cucumber.com' className='FixedTest' target='_self'>Link Here</a>
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </CardDeck>
+                    </div>
+                    <div className='space-line'>
+                    <CardDeck>
+                        <Card>
+                            <Card.Img variant="top" src="/images/WinDriLogo.png" />
+                            <Card.Body>
+                            <Card.Title>WinDriCraft</Card.Title>
+                            <Card.Text>
+                                A minecraft modpack project!<br></br>Rn in development
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="" />
+                            <Card.Body>
+                            <Card.Title>???</Card.Title>
+                            <Card.Text>
+                                ???
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="" />
+                            <Card.Body>
+                            <Card.Title>???</Card.Title>
+                            <Card.Text>
+                                ???<br></br>
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </CardDeck>
+                    </div>
+                </div>
+            </div>
+        </>
+    </Layout>
+  )
+}
