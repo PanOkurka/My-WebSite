de866ab13a7f0a4fa5a0761b43731416da53d568
Pan_Okurka <67430231+PanOkurka@users.noreply.github.com>
2022-02-12 09:03:10 +0100

    lazyload imgs and koadtime

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,17 @@ import Layout from '../components/layout'
 import { FaGithub, FaDiscord, FaTwitter, FaInstagram } from 'react-icons/fa'
 import Typing from '../components/typing'
 import ParticlesComponent from '../components/paticlecomponent'
+import Image from 'next/image'
+import Banner from '../public/images/Funny.gif'
+import present from 'present'
+import { IoIosTimer } from '@react-icons/all-files/io/IoIosTimer'
 
 export default function Home() {
+
+    const Time = present() / 1000;
+    var LoadTime = Time.toString().substring(0, 5)
+
+
   return (
     <Layout>
        <>
@@ -11,12 +20,20 @@ export default function Home() {
               <ParticlesComponent />
                 <div className='centerBox'> 
                     <div className='centerMain'>
-                        <img className='Banner' src='/images/Funny.gif' alt='Funny' />
+                        <div className='Banner'>
+                            <Image className='Banner' src={Banner} alt='Funny' />
+                        </div>
                         <div className='ContentMove'>
-                            <img className='Avatar' src="/images/profile.gif" alt="profile" />
+                            <div className='Avatar'>
+                                <Image className='Avatar' src="/images/profile.gif" alt='profile' height='200px' width="200px" />
+                            </div>
                             <div className='Center'>
                                 <h1 className='Name'>Pan_Okurka</h1>
                                 <h2 className='text'>I code in <Typing/></h2>
+                                <div className='customText'><h2><i>„Je dost těžké najít chybu v kódu, když ji hledáte; je to ještě těžší, když předpokládáte, že váš kód je bez chyb.“</i><br/><span className='podpis'>- Steve McConnell</span></h2></div>
+                                <div className='LoadTime'>
+                                    <IoIosTimer/><h4>{LoadTime}s</h4>
+                                </div>
                             </div>
                             <div className='Links'>
                                 <a href='https://github.com/PanOkurka' className='socialiconlist'>
