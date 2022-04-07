8401fc490f10275256306e35a77b6ec95c294a52
PanOkurka <tomiklima684@gmail.com>
2022-04-07 07:29:09 +0000

    feat: add basic Authors pages

--- pages/authors/[slug].js
+++ pages/authors/[slug].js
@@ -1,27 +1,37 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import Layout from '../../components/layout'
+import ParticlesComponent from '../../components/paticlecomponent'
+import { motion } from 'framer-motion'
 
 import { getAllAuthors, getAllPosts, getAuthorBySlug } from '../../lib/api'
 
 export default function Author({ author }) {
   return (
-    <div className="author">
-      <h1>{author.name}</h1>
+    <Layout>
+      <ParticlesComponent/>
+      <motion.div className="text-white absolute w-full left-0 flex flex-col items-center content-center text-center mt-10" exit={{opacity: 0, transition: {duration: 0.6}}} initial={{opacity: 0}} animate={{opacity: 1, transition: {duration: 0.6}}}>
+        <div className='AuthorPage'>
+          <h1 className='text-2xl border-b pb-2 mb-3'>{author.name}</h1>
 
-      <Image alt={author.name} src={author.profilePictureUrl} height="120" width="120" />
+          <div>
+            <Image alt={author.name} src={author.profilePictureUrl} className="f-img-border" height="120" width="120" />
+          </div>
 
-      <h2>Posts</h2>
+          <h2 className='text-2xl mt-4 border-b pb-1 mb-2'>Posts</h2>
 
-      <ul>
-        {author.posts.map(post => (
-          <li key={post.slug}>
-            <Link href={post.permalink}>
-              <a>{post.title}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+          <ul>
+            {author.posts.map(post => (
+              <li key={post.slug}>
+                <Link href={post.permalink}>
+                  <a className='hover:no-underline'>{post.title}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </motion.div>
+    </Layout>
   )
 }
 
--- pages/authors/index.js
+++ pages/authors/index.js
@@ -1,31 +1,37 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import Layout from '../../components/layout'
+import ParticlesComponent from '../../components/paticlecomponent'
+import { motion } from 'framer-motion'
 
 import { getAllAuthors, getAllPosts } from '../../lib/api'
 
 export default function Authors({ authors }) {
   return (
-    <div className="authors">
-      <h1>Authors</h1>
+    <Layout>
+      <ParticlesComponent/>
+      <motion.div className="text-white absolute w-full left-0 flex flex-col items-center content-center mt-14" exit={{opacity: 0, transition: {duration: 0.6}}} initial={{opacity: 0}} animate={{opacity: 1, transition: {duration: 0.6}}}>
+        <div className='flex flex-wrap w-1/2 flex-row items-center content-center'>
+          {authors.map(author => (
+            <div key={author.slug}>
+              <h2>
+                <Link href={author.permalink}>
+                  <a>{author.name}</a>
+                </Link>
+              </h2>
 
-      {authors.map(author => (
-        <div key={author.slug}>
-          <h2>
-            <Link href={author.permalink}>
-              <a>{author.name}</a>
-            </Link>
-          </h2>
+              <Image alt={author.name} src={author.profilePictureUrl} height="80" width="80" />
 
-          <Image alt={author.name} src={author.profilePictureUrl} height="80" width="80" />
+              <p>{author.posts.length} post(s)</p>
 
-          <p>{author.posts.length} post(s)</p>
-
-          <Link href={author.permalink}>
-            <a>Go to profile →</a>
-          </Link>
+              <Link href={author.permalink}>
+                <a>Go to profile →</a>
+              </Link>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </motion.div>
+    </Layout>
   )
 }
 
