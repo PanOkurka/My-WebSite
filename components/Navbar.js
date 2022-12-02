c1261479cb062ebd6411a89182993b0850c668b6
PanOkurka <okurka@baby-cucumber.com>
2022-12-02 11:30:46 +0000

    feat: changes to style

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,61 +7,65 @@ import { FaBook } from '@react-icons/all-files/fa/FaBook'
 import { BsFillInfoCircleFill } from '@react-icons/all-files/bs/BsFillInfoCircleFill'
 import { FaClipboardList } from '@react-icons/all-files/fa/FaClipboardList'
 import { AiOutlineFileSearch } from '@react-icons/all-files/ai/AiOutlineFileSearch'
+import { BsArrowLeftShort } from '@react-icons/all-files/bs/BsArrowLeftShort'
 export const Navbar = () => {
 
+   const [open, setOpen] = useState(false);
+
     return (
-      <aside className="h-screen fixed CustomSideBar z-30 w-60" aria-label="Sidebar">
+      <aside className={`h-screen fixed CustomSideBar duration-300 z-30 ${open? "w-60" : "w-20"}`} aria-label="Sidebar">
       <div className="overflow-y-auto h-screen py-4 px-3 rounded" style={{backgroundColor: '#23272A'}}>
+         <BsArrowLeftShort className={`bg-white text-black text-3xl rounded-full absolute -right-3 top-9 border border-black cursor-pointer ${!open && "rotate-180"}`} onClick={() => setOpen(!open)} />
          <Link href="/">
             <a className="flex pl-2.5 mb-5 CustomLogoSide hover:no-underline">
                   <img className="mr-3 h-9 CustomBorder" alt='logo' src='/images/profile.gif' />
-                  <span className="self-center text-lg font-semibold whitespace-nowrap text-white CustomTextSide hover:no-underline">Pan_Okurka</span>
+                  <span className={`self-center text-lg font-semibold whitespace-nowrap text-white CustomTextSide hover:no-underline ${!open && "scale-0"}`}>Pan_Okurka</span>
             </a>
          </Link>
          <ul className="space-y-2">
             <li>
                <Link href="/">
                   <a className="flex items-center p-2 text-base font-normal rounded-lg text-white hoverNavItem">
-                     <FaHome className="w-6 h-6 transition duration-75 text-gray-400 group-hover:text-white"></FaHome>
-                     <span className="ml-3 CustomTextSide">Home</span>
+                     <FaHome className={`text-3xl duration-75 text-gray-400`}></FaHome>
+                     <span className={`ml-10 absolute CustomTextSide duration-150 ${!open && "hidden scale-0 duration-75"}`}>Home</span>
                   </a>
                </Link>
             </li>
             <li>
                <Link href="/bio" >
                   <a className="flex items-center p-2 text-base font-normal rounded-lg text-white hoverNavItem">
-                     <FaBook className="w-6 h-6 transition duration-75 text-gray-400 group-hover:text-white"></FaBook>
-                     <span className="flex-1 ml-3 whitespace-nowrap CustomTextSide">Bio</span>
+                     <FaBook className="text-3xl transition duration-75 text-gray-400 group-hover:text-white"></FaBook>
+                     <span className={`flex-1 ml-10 absolute whitespace-nowrap CustomTextSide duration-150 ${!open && "hidden scale-0 duration-75"}`}>Bio</span>
                   </a>
                </Link>
             </li>
             <li>
                <Link href="/info">
                   <a className="flex items-center p-2 text-base font-normal rounded-lg text-white hoverNavItem">
-                     <BsFillInfoCircleFill className="w-6 h-6 transition duration-75 text-gray-400 group-hover:text-white"></BsFillInfoCircleFill>
-                     <span className="flex-1 ml-3 whitespace-nowrap CustomTextSide">Some Info</span>
+                     <BsFillInfoCircleFill className="text-3xl transition duration-75 text-gray-400 group-hover:text-white"></BsFillInfoCircleFill>
+                     <span className={`flex-1 ml-10 absolute whitespace-nowrap CustomTextSide duration-150 ${!open && "hidden scale-0 duration-75"}`}>Some Info</span>
                   </a>
                </Link>
             </li>
             <li>
                <Link href="/projects">
                   <a className="flex items-center p-2 text-base font-normal rounded-lg text-white hoverNavItem">
-                     <FaClipboardList className="w-6 h-6 transition duration-75 text-gray-400 group-hover:text-white"></FaClipboardList>
-                     <span className="flex-1 ml-3 whitespace-nowrap CustomTextSide">Projects</span>
+                     <FaClipboardList className="text-3xl transition duration-75 text-gray-400 group-hover:text-white"></FaClipboardList>
+                     <span className={`flex-1 ml-10 absolute whitespace-nowrap CustomTextSide duration-150 ${!open && "hidden scale-0 duration-75"}`}>Projects</span>
                   </a>
                </Link>
             </li>
             <li>
                <Link href="/ip">
                   <a className="flex items-center p-2 text-base font-normal rounded-lg text-white hoverNavItem">
-                     <AiOutlineFileSearch className="w-6 h-6 transition duration-75 text-gray-400 group-hover:text-white"></AiOutlineFileSearch>
-                     <span className="flex-1 ml-3 whitespace-nowrap CustomTextSide">Your IP</span>
+                     <AiOutlineFileSearch className="text-3xl transition duration-75 text-gray-400 group-hover:text-white"></AiOutlineFileSearch>
+                     <span className={`flex-1 ml-10 absolute whitespace-nowrap CustomTextSide duration-150 ${!open && "hidden scale-0 duration-75"}`}>Your IP</span>
                   </a>
                </Link>
             </li>
         </ul>
          <div className='Footer' id="Footer">
-          <div className="Middle">
+          <div className={`Middle duration-150 ${!open && "hidden scale-0 duration-75"}`}>
                     <h3 className='text-lg'>&copy; 2020-{new Date().getFullYear()} Pan_Okurka</h3>
                         <p className='infos mt-4'> 
                             <span className='mt-5 items-center content-center flex justify-center'><FaCodeBranch/> prod-2.0.0</span>
