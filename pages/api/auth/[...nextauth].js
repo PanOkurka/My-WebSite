0ded1cad98e9decd0d034976efe6df01ed3deef3
PanOkurka <okurka@baby-cucumber.com>
2022-12-25 16:48:21 +0000

    feat: migration to new auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,31 +1,16 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
-import CredentialsProvider from "next-auth/providers/credentials"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import prisma from "../../../lib/prismadb.ts"
 
 export const authOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENTID,
       clientSecret: process.env.GITHUB_SECRET
-    }),
-    CredentialsProvider({
-        name: 'Credentials',
-        credentials: {
-          username: { label: "Username", type: "text" },
-          email: { label: "E-Mail", type: "email" },
-          password: {  label: "Passworld", type: "password" }
-        },
-        async authorize(credentials, req) {
-         const {username, password, email} = credentials
-    
-          if (username === process.env.AUTH_USERNAME  && password === process.env.PASS && email === process.env.EMAIL) {
-            return {name: process.env.AUTH_USERNAME, email: process.env.EMAIL}
-          }
-
-          return null
-        }
-      })
+    })
   ],
   secret: process.env.JWT_SECRET,
+  adapter: PrismaAdapter(prisma),
 }
 export default NextAuth(authOptions)
