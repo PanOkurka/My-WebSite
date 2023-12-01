3f3960a174300a90c1acc18216333281132a0c74
PanOkurka <okurka@baby-cucumber.com>
2023-12-01 14:29:50 +0100

    feat: sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://dev.baby-cucumber.com/',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
